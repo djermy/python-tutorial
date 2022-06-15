c407844caaeec3f9d9c2501109ed4565cdd310be
Daniel Jermy <danjermy@yahoo.com>
2022-06-15 15:44:43 +0200

    added part of lesson 4

---
+++ lessons/4-maths-in-python/main.py
@@ -0,0 +1,9 @@
+# lesson 4
+# arithmatic operators: + - * / %(modulus, divides and returns the remainder) **(exponent, performs (power) calulation on operands) //(truncation/floor division)
+x = 5
+y = 2
+z = x + y
+
+print("Addition is:", z)
+print("Modulo is:",x % y)
+print("Floor Division is:", x // y)
